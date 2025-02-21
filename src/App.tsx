0278de9c4a356774f9e71135474345a58f4b1e5f
varun789-mx <varunsh153@gmail.com>
2025-02-22 01:14:26 +0530

    add card.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
+import { Card } from "./components/Card";
 import { ParticleBackground } from "./components/Particle";
 
 function App() {
   return (
     <>
-      <Navbar />
       <div>
-      <ParticleBackground />
+        <ParticleBackground />
+      </div>
+      <Navbar />
+      <div className="grid grid-cols-2 gap-2">
+        <div className="col-span-2 grid grid-cols-subgrid">
+          <Card />
+        </div>
+        <div className="col-span-2 grid grid-cols-subgrid">
+          <Card />
+        </div>
       </div>
     </>
   );
